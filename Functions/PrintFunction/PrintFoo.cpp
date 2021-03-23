1ab59f23eb6f3eb8087e4dcc9815f93662dd62e5
Slavchoo <tothelove1@gmail.com>
2021-03-23 20:36:58 +0200

    functs

--- Functions/PrintFunction/PrintFoo.cpp
+++ Functions/PrintFunction/PrintFoo.cpp
@@ -1,8 +1,64 @@
 #include "PrintFoo.h"
-
+#include <algorithm>
 
 template <typename PrintType>
 void Print(PrintType data)
 {
     std::cout << data << std::endl;
+}
+char get_ch(){
+    return std::cin.get();
+}
+std::string get(){
+    std::string in;
+    std::cin >> in;
+    return in;
+}
+std::string get_line(){
+    std::string in;
+    getline(std::cin, in);
+    return in;
+}
+namespace algo{
+    template <class T>
+    void sort(T &array, unsigned int first, unsigned int last){
+        std::sort(array+first, array + last);
+    }
+    template <class T>
+    void reverse(T &array, unsigned int first, unsigned int last){
+        std::reverse(array+first, array + last);
+    }
+    template <class T>
+    void swap(T &a, T &b){
+        T sw = b;
+        b = a;
+        a = sw;
+    }
+}
+namespace math{
+    const double PI = 3.14159265359;
+    const double EILER = 2,71828;
+    template <class T1, class T2>
+    T1 pow (T1 num, T2 times){
+        if(times == 0){
+            return 1;
+        }
+        else if(times > 0)
+        {
+            T1 result = num;
+            for(T2 i = 0; i < times; i++) result*=num;
+            return result;
+        }
+        else if(times < 0)
+        {
+            T1 result = num;
+            for(T2 i = 0; i < times; i++) result*=num;
+            return 1/result;
+        }
+    }
+    template <class T1>
+    double exp (T1 num){
+       return pow(EILER, num);
+    }
+
 }
