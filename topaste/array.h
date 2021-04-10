27ae00525aba0415cf9998725a77272dd9cb537a
Slavchoo <tothelove1@gmail.com>
2021-04-11 00:32:19 +0300

    array_updated

--- topaste/array.h
+++ topaste/array.h
@@ -10,7 +10,9 @@ class arr{
         std::map <std::string, std::vector<std::string>> Str;
     public:
     char get_type(std::string name){
-        return type[name];
+        if(type.find(name) != type.end())
+            return type[name];
+        return '0';
     }
     int get_int(std::string name, size_t index){
         return Int[name][index];
@@ -25,39 +27,44 @@ class arr{
         Int[name].push_back(val);
     }
     
-    void set_int(std::string name, std::vector<int> arr){
-        this->type[name] = 'i';
-        Int[name] = arr;
-    }
-     void set_bool(std::string name, bool val){
+    void set_bool(std::string name, bool val){
         Bool[name].push_back(val);
-    }
-    
-    void set_bool(std::string name, std::vector<bool> arr){
-        this->type[name] = 'b';
-        Bool[name] = arr;
     }
     void set_string(std::string name, std::string val){
         Str[name].push_back(val);
     }
     
-    void set_string(std::string name, std::vector<std::string> arr){
-        this->type[name] = 's';
-        Str[name] = arr;
-    }    
+    template <class ar>
+    void set(std::string name, std::vector <ar> arr){
+        ar type;
+        switch (sizeof(ar)){
+            case sizeof(int):
+                this->type[name] = 'i';
+                Int[name] = arr;
+                break;
+            case sizeof(bool):
+                this->type[name] = 'b';
+                Bool[name] = arr;
+                break;
+            case sizeof(std::string)
+                this->type[name] = 's';
+                Str[name] = arr;
+                break;
+        }
+    }
     void create(std::string name, char type){
         std::vector<int> created_int;
         std::vector<bool> created_bool;
         std::vector<std::string> created_string;
         switch (type){
             case 'i':
-                set_int(name, created_int);
+                set(name, created_int);
                 break;
             case 'b':
-                set_bool(name, created_bool);
+               set(name, created_bool);
                 break;
             case 's':
-                set_string(name, created_string);
+                set(name, created_string);
                 break;
         }
     }
@@ -73,6 +80,23 @@ class arr{
             case 's':
                 Str[name][index] = value;
                 break;
+        }
+    }
+    long size(std::string name){
+        switch(get_type(name)){
+            case 'i':
+                return Int[name].size();
+                break;
+            case 'b':
+                return Bool[name].size();
+                break;
+            case 's':
+                return Str[name].size();
+                break;
+            default:
+                return -1;
+                break;
+
         }
     }
 };
