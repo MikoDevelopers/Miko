35a925a88557a20ab2d4465b52490f9a5d78a0e4
bogdan-chernenko <bogdan.chernenko.018@gmail.com>
2021-04-13 14:34:15 +0300

    update builder

--- builder.py
+++ builder.py
@@ -1,37 +1,56 @@
 import subprocess
+import time
 import sys
 
-# builder.py -C
-# builder.py -R
-# builder.py -A
-# builder.py -V
+"""
+    *  builder.py -c, -C    only compile
+    *  builder.py -r, -R    run exe-file
+    *  builder.py -a, -A    start compile and after run compiled file
+    *  builder.py -v, -V    show version of this program
+    *  builder.pu -d, -D    start grogram with debug-mode(show time of working program)
+"""
 
 
-def compile_():
-    print("Start compile...")
+def showTime(func):
+    def wrapper():
+        startTime = time.time()
+        func()
+        endTime = time.time() - startTime
+        print(f"\n\n------------------------------------\n\tTime: {endTime}")
 
-    subprocess.call(f"{COMPILER} {' '.join(CPP_FILES)} -o {EXE_FILE}")
+    return wrapper
+
+
+class Builder:
+    def __init__(self):
+        ...
+
+    @staticmethod
+    def compile_():
+        print("Start compile...")
+
+        subprocess.call(f"{COMPILER} {' '.join(CPP_FILES)} -o {EXE_FILE}")
+        
+        print("Complie process is ended...")
     
-    print("Complie process is ended...")
+    @staticmethod
+    def run(filename = "main.miko"):
+        print("\nStart programm...\n------------------------\n\n")
 
+        subprocess.call(f"{EXE_FILE} {filename}")
 
-def run():
-    print("\nStart programm...\n------------------------\n\n")
-    
-    subprocess.call(EXE_FILE)
-
-
-def compile_run():
-    compile_()
-    run()
-
+    @staticmethod
+    @showTime
+    def compile_run():
+        Builder.compile_()
+        Builder.run()
 
 
 __version__ = 1.2
 AVAILBL_ARGS = {
-    "-C" : compile_,                                         # mode for only compile choised cpp-files
-    "-R" : run,                                              # mode for run compiled programm
-    "-A" : compile_run,                                      # compile programm and start
+    "-C" : Builder.compile_,                                         # mode for only compile choised cpp-files
+    "-R" : Builder.run,                                              # mode for run compiled programm
+    "-A" : Builder.compile_run,                                      # compile programm and start
     "-V" : lambda: print(f"Version: {__version__}"),         # show version 
 }
 ARGS = sys.argv
@@ -45,4 +64,4 @@ EXE_FILE = "main"                              # output exe-file
 
 
 
-AVAILBL_ARGS.get(ARGS[1])()
+AVAILBL_ARGS.get(ARGS[1])()                    # choice mode for program and start with choiced mode(s)
